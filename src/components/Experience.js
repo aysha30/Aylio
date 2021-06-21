33e64f4242a42f26f43a832801926907793e75f2
aysha30 <ettashamriya@gmail.com>
2021-06-21 21:57:37 +0530

    experience section complt

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { 
     Grid,
     Box,
+    CardContent,
     useMediaQuery,
+    Paper,
     Typography,} from '@material-ui/core';
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Fade from 'react-reveal/Fade';
+import uoodma from './Uoodma.png';
+import tsf from './tsf.png';
 
 const useStyle = makeStyles((theme) => ({
     root: {
@@ -17,16 +21,20 @@ const useStyle = makeStyles((theme) => ({
         width: "40%",
     },
     contentAbtMe:{
-        height: "100%",
+        height: "auto",
         [theme.breakpoints.down(1200)]:{
             padding:"50px"
         },
         [theme.breakpoints.up(1201)]:{
             padding: "100px 150px 100px 150px",
         },
+        display: "flex"
     },
-    paraAbtMe:{
-        width: "60%"
+    paper:{
+        width: "30%",
+    },
+    box:{
+        border: "5px",
     }
 }));
 
@@ -47,7 +55,63 @@ export default function Experience() {
                         </Fade>
                     </Typography>
                 </Grid>
-                <Grid item container>
+                <Grid item container direction="row" justify="space-evenly"
+                className={classes.contentAbtMe}
+                >
+                    <Grid item className={classes.paper}>
+                            <Fade bottom big>
+                            <Paper >
+                                <Box  border={4} borderRadius={10} borderColor="secondary.main" p={3} >
+                                    <Box align="center">
+                                        <img src={uoodma} alt={uoodma} />
+                                    </Box>
+                                    
+                                    <CardContent>
+                                        <Typography align="center">
+                                            <Box fontWeight="fontWeightBold">
+                                                Uoodmaish LLP
+                                            </Box>
+                                            <Box fontWeight="fontWeightMedium">
+                                                Web Developer <br/> (May, 2021 - Present)
+                                            </Box>
+                                            <Box fontWeight="fontWeightSmall">
+                                            Applying suitable algorithms to design a desired model for the given
+                                            datasets. Using Scikit-Learn library in machine learning to design
+                                            classification and regression models.
+                                            </Box>
+                                        </Typography>
+                                        
+                                    </CardContent>
+                                </Box>
+                                
+                            </Paper>
+                            </Fade>
+                        </Grid>
+                        <Grid item  className={classes.paper}>
+                        <Fade bottom big>
+                            <Paper component={Box} border={4} borderRadius={10} borderColor="secondary.main" p={3}>
+                                <Box align="center">
+                                <img src={tsf} alt={tsf} />
+                                </Box>
+                                <CardContent>
+                                    <Typography align="center">
+                                        <Box fontWeight="fontWeightBold">
+                                            The Sparks Foundation
+                                        </Box>
+                                        <Box fontWeight="fontWeightMedium">
+                                            Data Science Intern 
+                                            <br/>(Nov, 2020 - Dec, 2020)
+                                        </Box>
+                                        <Box fontWeight="fontWeightSmall">
+                                            Worked as a Web Developer at WebDriver, it's child Company.
+                                            Helped in website development with other team members.
+                                        </Box>
+                                    </Typography>
+                                    
+                                </CardContent>
+                            </Paper>
+                            </Fade>
+                        </Grid>
                     
                 </Grid>
                 
